83917a8d7d0bb9054b5d4b6a0a883633fadfc9bb
Philip <heroesfill@gmail.com>
2020-07-29 18:27:18 +0300

    Начал делать метод добавления новостей++

--- src/Views/new-news.php
+++ src/Views/new-news.php
@@ -2,7 +2,7 @@
 	<div class="info_buton conteiner">
         <h3 class="conteiner">Напишите новость</h3>
     </div>
-  	<form action="/addNewNews" >
+  	<form action="/addNews" method="POST">
 	  	<fieldset> 
 			<div class="anketa">
 				<label for="news_header">Название</label>
@@ -10,12 +10,12 @@
 			</div>
 		</fieldset> 
       	<fieldset> 
-		  <label for="article">Содержание</label>
+			<label for="article">Содержание</label>
 			<textarea name="article" id="" cols="30" rows="10" placeholder="Текст"></textarea> 
 		</fieldset> 
 	</form>
 	<div> 
 		<input type="submit" value="Добавить новость"> 
 	</div>
-	<? echo $newsResult;?>
+	<!-- <? echo $newsResult;?> -->
 </main>
